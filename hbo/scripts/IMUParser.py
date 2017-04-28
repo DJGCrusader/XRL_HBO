2c4a97bf1e530e8caf2010fa6a47a67376422f56
Daniel J. Gonzalez <dgonz@mit.edu>
2017-04-28 18:50:53 -0400

    IT WORKS

---
+++ hbo/scripts/IMUParser.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# IMU to joint_states publisher
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Vector3Stamped
+from std_msgs.msg import Header
+
+IMUState = [0,0,0, 0,0,0, 0,0,0]
+
+def IMUParser():
+    pub = rospy.Publisher('joint_states', JointState, queue_size=50)
+    rospy.init_node('IMUParser')
+    rate = rospy.Rate(50) # 10hz
+
+    hello_str = JointState()
+    hello_str.header = Header()
+    hello_str.header.stamp = rospy.Time.now()
+    hello_str.name = ['chest_yaw', 'chest_pitch', 'chest_roll',
+                      'right_shoulder_yaw', 'right_shoulder_pitch', 'right_shoulder_roll',
+                      'right_elbow']
+    subTo = ['/suit/chest/imu/rpy', '/suit/upper_right_arm/imu/rpy', '/suit/lower_right_arm/imu/rpy']
+    rospy.Subscriber(subTo[0], Vector3Stamped, IMUChestCallback)
+    rospy.Subscriber(subTo[1], Vector3Stamped, IMURUpperArmCallback)
+    rospy.Subscriber(subTo[2], Vector3Stamped, IMURLowerArmCallback)
+
+    hello_str.position = [IMUState[2], IMUState[1], IMUState[0], IMUState[5], IMUState[4], IMUState[3], IMUState[7]]
+    hello_str.velocity = []
+    hello_str.effort = []
+    while not rospy.is_shutdown():
+        hello_str = JointState()
+        hello_str.header = Header()
+        hello_str.header.stamp = rospy.Time.now()
+        hello_str.name = ['chest_yaw', 'chest_pitch', 'chest_roll',
+                          'right_shoulder_yaw', 'right_shoulder_pitch', 'right_shoulder_roll',
+                          'right_elbow']
+        hello_str.position = [IMUState[2], IMUState[1], IMUState[0], IMUState[5], IMUState[4], IMUState[3], IMUState[7]]
+        hello_str.velocity = []
+        hello_str.effort = []
+        pub.publish(hello_str)
+        # rate.sleep()
+
+def IMUChestCallback(data):
+    IMUState[0:3] = [data.vector.x, data.vector.y, data.vector.z]
+def IMURUpperArmCallback(data):
+    IMUState[3:6] = [data.vector.x, data.vector.y, data.vector.z]
+def IMURLowerArmCallback(data):
+    IMUState[6:9] = [data.vector.x, data.vector.y, data.vector.z]
+
+if __name__ == '__main__':
+    try:
+        IMUParser()
+    except rospy.ROSInterruptException:
+        pass
